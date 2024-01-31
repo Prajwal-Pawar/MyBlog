bd0a7bad8188a3b555b12644848f9a4a82797e0b
Prajwal <prajwalpawar2303@gmail.com>
2024-02-01 01:36:26 +0530

    reduce navbar width to 80%

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar flex flex-row justify-between m-5">
+    <div className="navbar w-4/5 flex flex-row justify-between m-auto mt-5">
       <div className="nav-left">
         <h1 className="text-xl font-bold hover:cursor-pointer">MyBlog</h1>
       </div>
