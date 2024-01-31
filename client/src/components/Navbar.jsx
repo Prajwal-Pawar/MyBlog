a85c65e347984afb279971464a9ac5ca68bd8eb7
Prajwal <prajwalpawar2303@gmail.com>
2024-01-31 19:49:58 +0530

    remove tailwindcss

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+const Navbar = () => {
+  return (
+    <div className="navbar flex flex-row">
+      <div className="left">
+        <h1 className="title">MyBlog</h1>
+      </div>
+
+      <div className="right">
+        <ul>
+          <li>sign up</li>
+          <li>sign in</li>
+        </ul>
+      </div>
+
+      <button className="">click</button>
+    </div>
+  );
+};
+
+export default Navbar;
