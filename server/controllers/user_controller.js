7fa3041859d9a69a3e31c86a4db2fc5bfa47dc9a
Prajwal <prajwalpawar2303@gmail.com>
2024-02-01 09:07:24 +0530

    add login controller and route

--- server/controllers/user_controller.js
+++ server/controllers/user_controller.js
@@ -1,6 +1,10 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
+const JWT_SECRET_KEY = "5uYwxKODsw34UtYagIZNGDo2GqnPY0tC";
+
+// register
 module.exports.signUp = async (req, res) => {
   try {
     let user = await User.findOne({ username: req.body.username });
@@ -25,6 +29,43 @@ module.exports.signUp = async (req, res) => {
 
     return res.status(200).json({
       message: "User registered successfully",
+    });
+  } catch (err) {
+    console.log(err);
+
+    return res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
+
+// login
+module.exports.login = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+
+    // if user doesnt exists
+    if (!user) {
+      return res.status(400).json({
+        message: "User doesnt exist",
+      });
+    }
+
+    // checking if inputted password and password in db matches
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(400).json({
+        message: "Incorrect password",
+      });
+    }
+
+    // generating jwt token to authorize user after login
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    return res.status(200).json({
+      message: "User logged in successfully",
+      token,
     });
   } catch (err) {
     console.log(err);
--- server/routes/user.js
+++ server/routes/user.js
@@ -6,4 +6,6 @@ const userController = require("../controllers/user_controller");
 
 router.post("/signup", userController.signUp);
 
+router.post("/login", userController.login);
+
 module.exports = router;
