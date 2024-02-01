880b0f7d3b9a2a8d3747bbc7fef5c650d40df9a5
Prajwal <prajwalpawar2303@gmail.com>
2024-02-01 13:37:23 +0530

    import AuthContext

--- client/src/providers/AuthProvider.jsx
+++ client/src/providers/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import AuthContext from "../context/AuthContext";
 
 export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(null);
