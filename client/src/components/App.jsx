4307130edbf3db4b7f16758488984fee1a7c1ebd
Prajwal <prajwalpawar2303@gmail.com>
2024-02-01 13:32:32 +0530

    add AuthContext and AuthProvider

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import SignUp from "../pages/SignUp";
 import Login from "../pages/Login";
+import Home from "../pages/Home";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
 
       <Routes>
+        <Route exact path="/" element={<Home />} />
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/login" element={<Login />} />
       </Routes>
---
+++ client/src/providers/AuthProvider.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    // get token from localstorage
+    const storedToken = localStorage.getItem("token");
+
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  // function to set the token and update local storage
+  const setAuthToken = (newToken) => {
+    setToken(null);
+    localStorage.setItem("token", newToken);
+  };
+
+  // function to remove the token and clear local storage after logout
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, setAuthToken, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
