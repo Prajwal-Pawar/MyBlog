01a538388b5637413b5803886fc93cb4723ec150
Prajwal <prajwalpawar2303@gmail.com>
2024-02-08 16:02:45 +0530

    fix updating Navbar after auth

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 
 const Navbar = () => {
   // get token from Auth Context
   const { token } = useContext(AuthContext);
+
+  const [authToken, setAuthToken] = useState(token);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    setAuthToken(localStorage.getItem("token"));
+  }, [location]);
 
   return (
     <div className="navbar w-4/5 flex flex-row justify-between m-auto mt-5">
@@ -13,7 +21,7 @@ const Navbar = () => {
       </div>
 
       <div className="">
-        {token ? (
+        {authToken ? (
           <ul className="flex flex-row">
             <li className="mr-5">
               <Link to={`/create-article`}>create</Link>
