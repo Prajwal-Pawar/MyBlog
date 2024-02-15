e15aa30c5dd8b478d3cf66fa744a8da8bc07de65
Prajwal <prajwalpawar2303@gmail.com>
2024-02-15 19:42:10 +0530

    update UserArticle page UI

--- client/src/pages/UserArticles.jsx
+++ client/src/pages/UserArticles.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import dayjs from "dayjs";
+import { MdDelete, MdEdit } from "react-icons/md";
 
 const UserArticles = () => {
   const [userArticles, setUserArticles] = useState([]);
@@ -33,31 +34,53 @@ const UserArticles = () => {
   }, []);
 
   return (
-    <div className="w-4/5 mt-8 flex flex-wrap m-auto">
+    <div className="container w-4/5 mt-8 mx-auto">
       <h1 className="text-lg mb-4 m-auto">
         You published <span className="font-bold">{userArticles.length}</span>{" "}
         articles till now.
       </h1>
 
-      {userArticles.map((article, index) => (
-        <Link
-          to={`/article/${article.slug}`}
-          className="w-full border mb-5 mr-4 p-5"
-          key={`article-${index}`}
-        >
-          <div>
-            <h1 className="text-2xl font-bold mb-1">{article.title}</h1>
-            <div className="flex justify-between">
-              <h3 className="text-base text-slate-600">
+      <div className="flex flex-wrap -mx-4">
+        {userArticles.map((article, index) => (
+          <div
+            key={`article-${index}`}
+            className="w-full md:w-1/2 lg:w-1/3 px-4 mb-4"
+          >
+            <div className="border p-4 rounded-md">
+              <Link to={`/article/${article.slug}`}>
+                <h2 className="text-lg font-semibold mb-2">{article.title}</h2>
+              </Link>
+
+              <p className="text-sm text-slate-600 mb-2">
                 {article.description}
-              </h3>
-              <p className="text-base text-slate-600">
-                {dayjs(article.createdAt).format("DD MMM YYYY")}
               </p>
+
+              <div className="flex justify-between items-center">
+                <p className="text-sm text-slate-600">
+                  {dayjs(article.createdAt).format("DD MMM YYYY")}
+                </p>
+
+                <div>
+                  <Link to={`/article/edit/${article._id}`}>
+                    <button className="mr-2 py-1 px-2 font-semibold rounded shadow bg-indigo-600 hover:bg-indigo-500 text-white focus:shadow-outline focus:outline-none">
+                      {/* icon */}
+                      <MdEdit className="inline-block mr-1" size={20} />
+                      Edit
+                    </button>
+                  </Link>
+                  <Link>
+                    <button className="mr-2 py-1 px-2 font-semibold rounded shadow bg-red-600 hover:bg-red-500 text-white focus:shadow-outline focus:outline-none">
+                      {/* icon */}
+                      <MdDelete className="inline-block mr-1" size={20} />
+                      Delete
+                    </button>
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
-        </Link>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
