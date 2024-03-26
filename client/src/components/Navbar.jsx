f27be79e1397b1786495a54cde47b608fefd9dd2
Prajwal Pawar <prajwalpawar2303@gmail.com>
2024-03-27 00:01:11 +0530

    add searchbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { MdSearch } from "react-icons/md";
 
 const Navbar = () => {
   // get token from localstorage
@@ -21,26 +22,38 @@ const Navbar = () => {
   }, [location, authToken]);
 
   return (
-    <div className="navbar w-4/5 flex flex-row justify-between m-auto mt-5">
+    <div className="navbar w-4/5 flex flex-row justify-between items-center m-auto mt-5">
       <div className="nav-left">
         <Link to="/">
           <h1 className="text-xl font-bold hover:cursor-pointer">MyBlog</h1>
         </Link>
       </div>
 
-      <div className="">
+      <div>
         {authToken ? (
-          <ul className="flex flex-row">
-            <li className="mr-5">
-              <Link to="/article/create">create</Link>
-            </li>
-            <li className="mr-5">
-              <Link to={`/user/profile`}>profile</Link>
-            </li>
-            <li className="mr-5">
-              <Link onClick={logout}>logout</Link>
-            </li>
-          </ul>
+          <div className="flex justify-center items-center">
+            {/* search bar */}
+            <div className="flex items-center border border-gray-300 rounded-md px-3 py-1 mr-4">
+              <MdSearch className="h-6 w-6 mr-2 text-gray-600" />
+              <input
+                type="text"
+                placeholder="Search articles.."
+                className="outline-none placeholder-gray-500 flex-grow"
+              />
+            </div>
+
+            <ul className="flex flex-row">
+              <li className="mr-5 hover:text-blue-700">
+                <Link to="/article/create">create</Link>
+              </li>
+              <li className="mr-5 hover:text-blue-700">
+                <Link to={`/user/profile`}>profile</Link>
+              </li>
+              <li className="mr-5 hover:text-blue-700">
+                <Link onClick={logout}>logout</Link>
+              </li>
+            </ul>
+          </div>
         ) : (
           <ul className="flex flex-row">
             <li className="mr-5">
